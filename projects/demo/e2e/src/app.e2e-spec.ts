ad607aa472f6217c1a774c7ae88fd0d3447d48ae
Renaud Lenoir <renaud.lenoir@gmail.com>
2019-07-26 11:15:43 +0200

    Added e2e test for demo app in CircleCi config

--- projects/demo/e2e/src/app.e2e-spec.ts
+++ projects/demo/e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display toolbar title', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to demo!');
+    expect(page.getToolbarTitleText()).toEqual('Angular Libraries - Demo');
   });
 
   afterEach(async () => {
