eedbc896862200dd05da8105cab982364bdc293e
Renaud Lenoir <renaud.lenoir@gmail.com>
2019-08-01 08:31:23 +0200

    Added code coverage tresholds

--- projects/template/karma.conf.js
+++ projects/template/karma.conf.js
@@ -21,8 +21,13 @@ module.exports = function (config) {
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../../coverage/template'),
       reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
-    },
+      fixWebpackSourcePaths: true,
+      thresholds: {
+        statements: 80,
+        lines: 80,
+        branches: 80,
+        functions: 80
+      }    },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
